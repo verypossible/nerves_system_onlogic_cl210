670fd43814db7d6656f57d11a05931a502cb040c
Justin Schneck <jschneck@mac.com>
2021-02-11 09:07:05 -0500

    bump deps

--- mix.exs
+++ mix.exs
@@ -52,8 +52,8 @@ defmodule NervesSystemOnLogicCL210.MixProject do
       {:nerves, "~> 1.5.4 or ~> 1.6.0 or ~> 1.7.0", runtime: false},
       {:nerves_system_br, "1.14.4", runtime: false},
       {:nerves_toolchain_x86_64_nerves_linux_gnu, "~> 1.4.0", runtime: false},
-      {:nerves_system_linter, "~> 0.3.0", runtime: false},
-      {:ex_doc, "~> 0.18", only: [:dev, :test], runtime: false}
+      {:nerves_system_linter, "~> 0.4", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.22", only: :docs, runtime: false}
     ]
   end
 
